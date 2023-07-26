fd03af82596e8592fc186b01d233c2d5202baf82
Fábio <fabiosgcontato@gmail.com>
2023-07-26 16:39:15 -0300

    ImageUpload on RentModal

--- app/components/Modals/RentModal/index.tsx
+++ app/components/Modals/RentModal/index.tsx
@@ -10,6 +10,7 @@ import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../../Inputs/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../../Inputs/Counter';
+import ImageUpload from '../../Inputs/ImageUpload';
 
 type Props = {}
 
@@ -54,6 +55,7 @@ const RentalModal = (props: Props) => {
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount');
   const bathroomCount = watch('bathroomCount');
+  const imageSrc = watch('imageSrc');
 
   const Map = useMemo(() => dynamic(() => import('../../Map'), {
     ssr: false
@@ -157,6 +159,21 @@ const RentalModal = (props: Props) => {
           subtitle="How many bathrooms do you have?"
           value={bathroomCount}
           onChange={(value) => setCustomValue('bathroomCount', value)}
+        />
+      </div>
+    )
+  }
+
+  if(step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading 
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like"
+        />
+        <ImageUpload
+          onChange={(value) => setCustomValue('imageSrc', value)}
+          value={imageSrc}
         />
       </div>
     )
